0c41833ef4fb56242377b7bc7a4610fb98a94b48
winalike5104 <winalike5104@users.noreply.github.com>
2026-07-10 22:52:06 +1200

    Add Vercel Blob media asset manifest

---
+++ lib/blob-media-assets.ts
@@ -0,0 +1,84 @@
+const blobBaseUrl = "https://p5cvrz9hmwqxi7ak.public.blob.vercel-storage.com/website-media";
+
+export const blobMediaAssets = {
+  images: [
+    {
+      name: "nexautoclip1",
+      url: `${blobBaseUrl}/nexautoclip1.png`
+    },
+    {
+      name: "nexautoclip11",
+      url: `${blobBaseUrl}/nexautoclip11.png`
+    },
+    {
+      name: "nexautoclip13",
+      url: `${blobBaseUrl}/nexautoclip13.png`
+    },
+    {
+      name: "nexautoclip2",
+      url: `${blobBaseUrl}/nexautoclip2.png`
+    },
+    {
+      name: "nexautoclip4",
+      url: `${blobBaseUrl}/nexautoclip4.png`
+    },
+    {
+      name: "nexautoclip7",
+      url: `${blobBaseUrl}/nexautoclip7.png`
+    },
+    {
+      name: "nexautoclip8",
+      url: `${blobBaseUrl}/nexautoclip8.png`
+    },
+    {
+      name: "nexautowiper1",
+      url: `${blobBaseUrl}/nexautowiper1.png`
+    },
+    {
+      name: "nexautowiper2",
+      url: `${blobBaseUrl}/nexautowiper2.png`
+    },
+    {
+      name: "nexautowiper3",
+      url: `${blobBaseUrl}/nexautowiper3.png`
+    },
+    {
+      name: "nexautowiper4",
+      url: `${blobBaseUrl}/nexautowiper4.png`
+    },
+    {
+      name: "nexautowiper5",
+      url: `${blobBaseUrl}/nexautowiper5.png`
+    }
+  ],
+  videos: [
+    {
+      name: "topstar-01",
+      url: `${blobBaseUrl}/topstar-01.mp4`
+    },
+    {
+      name: "topstar-02",
+      url: `${blobBaseUrl}/topstar-02.mp4`
+    },
+    {
+      name: "topstar-04",
+      url: `${blobBaseUrl}/topstar-04.mp4`
+    },
+    {
+      name: "topstar-07",
+      url: `${blobBaseUrl}/topstar-07.mp4`
+    },
+    {
+      name: "topstar-08",
+      url: `${blobBaseUrl}/topstar-08.mp4`
+    },
+    {
+      name: "topstar-11",
+      url: `${blobBaseUrl}/topstar-11.mp4`
+    },
+    {
+      name: "topstar-13",
+      url: `${blobBaseUrl}/topstar-13.mp4`
+    }
+  ]
+} as const;
